2f6b9efa3932d481bf3d9984a42b795b17271075
owain perry <owain.perry@just-eat.com>
2017-07-20 12:32:36 +0000

    correct hostname

--- install_schema.sh
+++ install_schema.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-#HOST="google.com"
+#HOSTNAME="google.com"
 #PORT=443
 COUNTER=30
 PORTOPEN=false
 env
 
+echo "Checking   HOSTNAME: $HOSTNAME port: $PORT"
+
 if [ -z "$PORT" ]; then PORT=3306; fi
-if [ -z "$HOST" ]; then HOST=localhost; fi
+if [ -z "$HOSTNAME" ]; then HOSTNAME=localhost; fi
 if [ -z "$USERNAME" ]; then USERNAME=piapp; fi
 if [ -z "$PASSWORD" ]; then PASSWORD=abc2233; fi
 if [ -z "$TESTDATA" ]; then TESTDATA=false; fi
 
-echo "Checking host: $HOST port: $PORT"
+echo "Checking HOSTNAME: $HOSTNAME port: $PORT"
 
 until [  $COUNTER -lt 1 ]; do    
-    nc -z $HOST $PORT
+    nc -z $HOSTNAME $PORT
     if [ $? -eq 0 ]; then
    		echo "Discovered port $PORT open"
    		let COUNTER=-1
@@ -27,13 +29,13 @@ done
 
 #check the port is open 
 if $PORTOPEN; then
-   	echo "$HOST Port $PORT  is open"
-   	mysql -h $HOST-u $USERNAME -p $PASSWORD --port=$PORT < schema.sql
+   	echo "hostname: $HOSTNAME Port $PORT is open"
+   	mysql -h $HOSTNAME-u $USERNAME -p $PASSWORD --port=$PORT < schema.sql
    	if $TESTDATA; then
-		mysql -h $HOST-u $USERNAME -p $PASSWORD --port=$PORT < test_data.sql
+		mysql -h $HOSTNAME-u $USERNAME -p $PASSWORD --port=$PORT < test_data.sql
    	fi
 else
-   	echo "$HOST Port $PORT not open in time"
+   	echo "hostname $HOSTNAME Port $PORT not open in time"
 	exit 1;
 fi 
 
