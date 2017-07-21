731e04438f855ca03662b90e5e5846302b56ac4f
owain perry <owain.perry@just-eat.com>
2017-07-21 08:49:36 +0000

    using full DNS address bypasses the problem

--- install_schema.sh
+++ install_schema.sh
@@ -16,33 +16,33 @@ if [ -z "$TESTDATA" ]; then TESTDATA=false; fi
 if [ -z "$POD_NAMESPACE" ]; then 
   FQDN="$HOSTNAME"
 else 
-  FQDN="$HOSTNAME.$POD_NAMESPACE" 
+  FQDN="$HOSTNAME.$POD_NAMESPACE.svc.cluster.local" 
 fi
 
 echo "Checking FQDN: $FQDN port: $PORT"
 
- 
-
-until [  $COUNTER -lt 1 ]; do    
+#until [  $COUNTER -lt 1 ]; do    
     #nc -z "$FQDN" $PORT
-       ping $FQDN 
+    #   ping $FQDN 
     #if [ $? -eq 0 ]; then
 #   		echo "Discovered port $PORT open"
 #   		let COUNTER=-1
    		PORTOPEN=true
    #	else
    #	  echo "exit was : $?"
-    	sleep 1
+#    	sleep 1
 #	fi
-    let COUNTER-=1
-done
+#    let COUNTER-=1
+#done
 
 #check the port is open 
 if $PORTOPEN; then
    	echo "hostname: $FQDN Port $PORT is open"
-   	mysql --host=$FQDN --username="$USERNAME" --password="$PASSWORD" --port="$PORT" < schema.sql
+   	mysql -h $FQDN -u"$USERNAME" --password="$PASSWORD" < schema.sql
+    echo "success running schema $?"
    	if $TESTDATA; then
-		#mysql -host=$FQDN --username="$USERNAME" -p$PASSWORD --port="$PORT" < test_data.sql
+		    mysql -h $FQDN -u"$USERNAME" --password="$PASSWORD"  < test_data.sql
+        echo "success adding test data $?"
    	fi
 else
   echo "hostname $FQDN Port $PORT not open in time"
