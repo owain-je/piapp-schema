8aa2b5a58d481d19630b9538b666da9f6a37048d
owain perry <owain.perry@just-eat.com>
2017-11-01 10:34:03 +0000

    updated environment variables

--- install_schema.sh
+++ install_schema.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
-#HOSTNAME="google.com"
-#PORT=443
 COUNTER=1200
 PORTOPEN=false
 env
 
-echo "Checking HOSTNAME: $HOSTNAME port: $PORT"
+echo "Checking MYSQL_HOSTNAME: $MYSQL_HOSTNAME port: $PORT"
 
-if [ -z "$PORT" ]; then PORT=3306; fi
-if [ -z "$HOSTNAME" ]; then HOSTNAME=localhost; fi
-if [ -z "$USERNAME" ]; then USERNAME=piapp; fi
-if [ -z "$PASSWORD" ]; then PASSWORD=abc2233; fi
+if [ -z "$MYSQL_PORT" ]; then MYSQL_PORT=3306; fi
+if [ -z "$MYSQL_HOSTNAME" ]; then MYSQL_HOSTNAME=localhost; fi
+if [ -z "$MYSQL_USERNAME" ]; then USERNAME=root; fi
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then MYSQL_ROOT_PASSWORD=abc2233; fi
 if [ -z "$TESTDATA" ]; then TESTDATA=false; fi
 
 if [ -z "$POD_NAMESPACE" ]; then 
-  FQDN="$HOSTNAME"
+  FQDN="$MYSQL_HOSTNAME"
 else 
-  FQDN="$HOSTNAME.$POD_NAMESPACE.svc.cluster.local" 
+  FQDN="$MYSQL_HOSTNAME.$POD_NAMESPACE.svc.cluster.local" 
 fi
 
 echo "Checking FQDN: $FQDN port: $PORT"
 
 #until [  $COUNTER -lt 1 ]; do    
-    #nc -z "$FQDN" $PORT
+    #nc -z "$FQDN" $MYSQL_PORT
     #   ping $FQDN 
     #if [ $? -eq 0 ]; then
-#   		echo "Discovered port $PORT open"
+#   		echo "Discovered port $MYSQL_PORT open"
 #   		let COUNTER=-1
    		PORTOPEN=true
    #	else
@@ -37,15 +35,15 @@ echo "Checking FQDN: $FQDN port: $PORT"
 
 #check the port is open 
 if $PORTOPEN; then
-   	echo "hostname: $FQDN Port $PORT is open"
-   	mysql -h $FQDN -u"$USERNAME" --password="$PASSWORD" < schema.sql
+   	echo "MYSQL_HOSTNAME: $FQDN Port $MYSQL_PORT is open"
+   	mysql -h $FQDN -u"$MYSQL_USERNAME" --password="$MYSQL_ROOT_PASSWORD" < schema.sql
     echo "success running schema $?"
    	if $TESTDATA; then
-		    mysql -h $FQDN -u"$USERNAME" --password="$PASSWORD"  < test_data.sql
+		    mysql -h $FQDN -u"$MYSQL_USERNAME" --password="$MYSQL_ROOT_PASSWORD"  < test_data.sql
         echo "success adding test data $?"
    	fi
 else
-  echo "hostname $FQDN Port $PORT not open in time"
+  echo "MYSQL_HOSTNAME $FQDN Port $MYSQL_PORT not open in time"
 	exit 1
 fi 
 
